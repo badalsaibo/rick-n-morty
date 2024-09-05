66506b23336abd2c717bb6b4edd0fc7d7b0586a1
Badal Saibo <develop.badal@gmail.com>
2024-09-05 22:49:06 +0530

    feat: bootsplash

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
